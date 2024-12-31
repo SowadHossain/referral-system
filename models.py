a64fd5555e88ec6c983a30295b854be2351c314b
Sowad <sowadhossain017@gmail.com>
2024-12-31 13:11:00 +0600

    base

--- models.py
+++ models.py
@@ -1,6 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    referral_code = db.Column(db.String(10), unique=True, nullable=False)
+    referrals_count = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Referral(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    referrer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    referred_email = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 def init_app(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
---
+++ referral.py
@@ -0,0 +1,13 @@
+from models import db, User
+
+import random
+import string
+
+def generate_referral_code(length=8):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
+def increment_referrals_count(referrer_id):
+    referrer = User.query.filter_by(id=referrer_id).first()
+    if referrer:
+        referrer.referrals_count += 1
+        db.session.commit()
